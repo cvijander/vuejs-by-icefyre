3220494681da7e56f7d1e852f2ab0aaa7bd34492
cvijander <cvija85@gmail.com>
2024-08-27 13:19:39 +0200

    Cards

--- projekat/src/App.vue
+++ projekat/src/App.vue
@@ -1,10 +1,31 @@
 <script>
+import Card from './components/Card.vue';
+
 export default {
   name: "App",
+  components: {Card},
   data() {
     return {
       name: "dejan",
       age: 29,
+      cards: [
+        {
+          id: 1,
+          title: "Kartica 1",
+          text: "Tekst 1" 
+        },
+        {
+          id: 2,
+          title: "Kartica 2",
+          text: "Tekst 2" 
+        },
+        {
+          id: 3,
+          title: "Kartica 3",
+          text:"Tekst 3" 
+        }
+      ]
+      
       // isShow: false,
       // loremText: 'lorem dolor ipsuim ..',
       // someHtml : '<h1>Ovo je naslov</h1>',
@@ -39,7 +60,7 @@ export default {
       </li>
     </ul>    
   </div> -->
-
-
-
+   <div v-for="card in cards" :key="card.id">
+      <Card :id="card.id" :title="card.title" :text="card.text"/> 
+    </div> 
 </template>
