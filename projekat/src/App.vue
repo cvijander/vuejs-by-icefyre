da6fc2a6e48003a615889b05f8fe94b1a0161a5f
cvijander <cvija85@gmail.com>
2024-08-27 12:19:23 +0200

    Pravimo projektat

---
+++ projekat/src/App.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "dejan",
+      age: 29,
+      // isShow: false,
+      // loremText: 'lorem dolor ipsuim ..',
+      // someHtml : '<h1>Ovo je naslov</h1>',
+      // books: [
+      //   {
+      //     id: 1,
+      //     name: 'dejan'
+      //   },
+      //   {
+      //     id: 2,
+      //     name: 'pera'
+      //   },
+      //   {
+      //     id: 3,
+      //     name: 'dejan'
+      //   }
+      // ]
+      
+    };
+  },
+};
+</script>
+
+<template>
+ <!-- <div > 
+    <p v-text="loremText"></p>
+    <p v-html="someHtml"></p>
+    <ul>
+      <li v-for="book in books" :key="book.id">
+        {{book.id  }}
+        {{book.name}}
+      </li>
+    </ul>    
+  </div> -->
+
+
+
+</template>
